090090ce939c323ec9792b8f3bf95c24a7456491
frimpsss <afrimpong031@gmail.com>
2023-10-18 19:21:23 +0000

    change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Frimps',
+  title: 'frimps.xyz',
   description: 'Bants and Rants',
   icons: "./favicon.ico"
 }
